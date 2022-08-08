be92281ee4bd068b678a281ed7206aa0a39cf051
Archana.Su <Archana.Su@EXIND3010-35.allfon.in>
2022-08-08 14:06:33 +0530

    2

---
+++ book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Book;
+import com.example.demo.entity.CabDriver;
+import com.example.demo.service.BookService;
+
+@RestController
+@RequestMapping(path="/api/v1")
+public class BookController {
+	
+	private BookService service;
+	
+    @Autowired
+	public BookController(BookService service) {
+		super();
+		this.service = service;
+	}
+    
+
+	@PostMapping
+	public Book add(@RequestBody Book entity) {
+		return this.service.save(entity);
+	}
+    @GetMapping(path="/books")
+    public List<Book> findAll(){
+    	return this.service.findAll();
+    }
+	
+	
+
+}
